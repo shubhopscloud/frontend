1ad0c3335ae47989ffbc253d089b6c3a4c02a997
Ubuntu <ubuntu@ip-172-31-27-122.us-east-2.compute.internal>
2023-10-15 17:29:17 +0000

    added code

---
+++ build/precache-manifest.636c9804be1689c5a98ae67d0b95518d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5fca24fb7213dc2e7cd2",
+    "url": "/static/css/main.e7ac6953.chunk.css"
+  },
+  {
+    "revision": "5fca24fb7213dc2e7cd2",
+    "url": "/static/js/main.64e28203.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fa312d4166e5af4b3504",
+    "url": "/static/css/2.9caff158.chunk.css"
+  },
+  {
+    "revision": "fa312d4166e5af4b3504",
+    "url": "/static/js/2.360a3732.chunk.js"
+  },
+  {
+    "revision": "f24b133ed4406bb17876c3ed7fa86f22",
+    "url": "/static/media/tabler-webfont.f24b133e.eot"
+  },
+  {
+    "revision": "b56d2ee6c65e0104e4ef76c027587e46",
+    "url": "/static/media/tabler-webfont.b56d2ee6.woff2"
+  },
+  {
+    "revision": "924bfe321562eb8cdedf675ad0e011b3",
+    "url": "/static/media/tabler-webfont.924bfe32.woff"
+  },
+  {
+    "revision": "68520a6b16e40c7b3538bae7c43eb776",
+    "url": "/static/media/tabler-webfont.68520a6b.ttf"
+  },
+  {
+    "revision": "ad55e63e508f5b0e55e9d2394a2732c7",
+    "url": "/static/media/tabler-webfont.ad55e63e.svg"
+  },
+  {
+    "revision": "1e56dc9f731d1a59f88dea1f651d2f9f",
+    "url": "/index.html"
+  }
+];
